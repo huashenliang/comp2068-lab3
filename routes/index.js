3c04e648a14ad0cf8771700849a6da315745573c
Huashen Liang <JohnnyLiang77@gmail.com>
2019-06-03 13:50:44 -0400

    Creation of routes, huasheng.pug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/huashen', (req, res, next) => {
+  res.render('huashen', {});
+});
+
+router.get('/mysister', (req, res, next) => {
+  res.render('sister', {});
+});
+
+router.get('/father', (req, res, next) => {
+  res.render('father', {});
+});
+
+router.get('/mother', (req, res, next) => {
+  res.render('mother', {});
+});
+
+router.get('/dog', (req, res, next) => {
+  res.render('dog', {});
+});
+
 module.exports = router;
